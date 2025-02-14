19bd556b47c6a0a8c34853aa1f2e89287d34979a
Sebastian <sebander01@gmail.com>
2025-02-14 00:03:42 -0500

    BringUpCardUIForPuzzleArea

---
+++ Assets/Scripts/ManagerAndControllers/PuzzleManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+
+    private GameObject CombatUI;
+    //Make an instance
+    public static PuzzleManager Instance
+    {
+        get;
+
+        set;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Potentially needs to be modified for skipping tutorial
+        if (GameManager.Instance.CurrentLevel == Levels.Tutorial)
+        {
+            //It is possible to load this script before this object exits
+            try
+            {
+                CombatUI = GameObject.Find("UiManager").transform.Find("Roaming And Combat UI").gameObject;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// This method called from PuzzleRange collison opens the combat UI so the puzzle is possible.
+    /// </summary>
+    public void OpenPuzzle()
+    {
+        Debug.Log("Opened");
+        //Open combat UI
+        CombatUI.GetComponent<RoamingAndCombatUiController>().SwitchMode(true);
+    }
+
+    /// <summary>
+    /// This method called from PuzzleRange collison closes the combat UI so the puzzle is impossble
+    /// </summary>
+    public void ClosePuzzle()
+    {
+        Debug.Log("Cosed");
+        //Close combat UI
+
+        CombatUI.GetComponent<RoamingAndCombatUiController>().SwitchMode(false);
+    }
+}
