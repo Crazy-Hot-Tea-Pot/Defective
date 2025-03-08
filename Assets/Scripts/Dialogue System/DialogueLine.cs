aef3ec180d5ad7a8dc51eeacc1c978e2dd021e48
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2025-03-08 01:02:42 -0500

    Start Creating Dialogue system

---
+++ Assets/Scripts/Dialogue System/DialogueLine.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueLine
+{
+    // NPC name
+    public string speakerName;
+
+    // NPC image
+    public Sprite speakerImage;
+
+    // Text content
+    [TextArea(3, 5)]
+    public string dialogueText;
+
+    // If true, uses PlayerSpeak()
+    public bool isPlayerSpeaking;
+
+    // Letter-by-letter (default) or Word-by-word
+    public bool revealByLetter = true;
+
+    // Only for Player dialogue
+    public float textSpeed = 0.05f;
+}
